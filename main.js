e14a8fe8d453ff4aa1718fc09da33c6fdcd83ac6
Donuts_n_Tiaras <61537524+donutsntiaras@users.noreply.github.com>
2020-10-20 15:29:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ image_format:'png',
 png_quality:90,
 
    constraints: {
-facing_mode:environment
+facing_mode:"environment"
  }
 });
 
@@ -14,7 +14,7 @@ Webcam.attach('#camera');
 
 function take_snapshot() {
     Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+        document.getElementById("result").innerHTML = ' <img id="captured_image" src=" ' +data_uri+ ' "> ';
     });
 }
 
